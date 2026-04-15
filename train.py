70ef0a937c377b4405e79ff80d1ca4f7a6dbe7d7
Prajin <patilprajin3@gmail.com>
2026-04-15 22:27:33 +0530

    train-1

---
+++ train.py
@@ -0,0 +1,11 @@
+#start
+# Load Data, set configuration variables
+item_train, user_train, y_train, item_features, user_features, item_vecs, movie_dict, user_to_genre = load_data()
+
+num_user_features = user_train.shape[1] - 3  # remove userid, rating count and ave rating during training
+num_item_features = item_train.shape[1] - 1  # remove movie id at train time
+uvs = 3  # user genre vector start
+ivs = 3  # item genre vector start
+u_s = 3  # start of columns to use in training, user
+i_s = 1  # start of columns to use in training, items
+print(f"Number of training vectors: {len(item_train)}")
